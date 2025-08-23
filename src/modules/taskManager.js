b23087d156b6e488c3150f8f25ccd46c7c2ba884
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-23 23:36:20 +0300

    feat: add taskManager with Project and Task funcs

---
+++ src/modules/taskManager.js
@@ -0,0 +1,39 @@
+const projects = [];
+
+class Project {
+    constructor(name) {
+        this.name = name;
+        this.id = crypto.randomUUID();
+        this.tasks = [];
+        projects.push(this);
+    }
+
+    addTask(task) {
+        this.tasks.push(task);
+    }
+
+    static getProjects() {
+        return projects;
+    }
+
+    removeTask(taskId) {
+        const task = this.tasks.find(
+            task => task.id === taskId
+        );
+        const index = this.tasks.indexOf(task);
+        this.tasks.slice(index, 1);
+    }
+}
+
+class Task {
+    constructor(title, description, priority, dueDate) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+        this.dueDate = dueDate;
+        this.completed = false;
+        this.id = crypto.randomUUID();
+    }
+}
+
+export {Task, Project};
