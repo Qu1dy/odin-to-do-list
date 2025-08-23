5ad489b0ba76fd67dd7ffe75eb76775e1e7d95e6
Qu1dy <71177869+Qu1dy@users.noreply.github.com>
2025-08-23 23:41:22 +0300

    style: change task color based on priority

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-import "./style.css";
+import "./style.css"; 
+import renderer from "./modules/renderer.js";
+import {Task, Project} from "./modules/taskManager.js"; 
+
+(() => {
+    const p1 = new Project("meow");
+    const task1 = new Task("meow meow", "", "high", "07/04/2025", true);
+    console.log(task1);
+    p1.addTask(task1);
+    const projects = Project.getProjects();
+    renderer.renderProjects(projects);
+})();
